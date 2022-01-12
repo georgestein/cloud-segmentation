d26a0ba0ea0f42c7e96aaffa1c2bf5f64b557dde
Dana Simard <dana.simard@astro.caltech.edu>
2022-01-11 21:23:09 -0800

    No dash in name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='cloud-segmentation',
+    name='cloud_segmentation',
     version="0.0",
     url='http://github.com/gstein/cloud-segmentation/',
     author="George Stein",
