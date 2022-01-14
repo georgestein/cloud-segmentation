1cdbd1694ce0771e23ebf48889f3310689690885
georgestein <george.f.stein@gmail.com>
2022-01-14 13:26:06 -0800

    latest updates

--- setup.py
+++ setup.py
@@ -6,10 +6,8 @@ setup(
     url='http://github.com/gstein/cloud-segmentation/',
     author="George Stein",
     author_email='george.f.stein@gmail.com',
-    packages=find_packages(), #['cloud_seg, cloud_seg.pc_apis'], #find_packages(exclude=["tests", "docs", "scripts"]),
-    package_data={
-        'cloud_seg': [],
-        },
+    packages=find_packages(), 
+    include_package_data=True
     install_requires=[],
     dependency_links=[]
 )
