2c0eaf435a7112e1dfce2c5bb9b561e0f2f3623b
Dana Simard <dana.simard@astro.caltech.edu>
2022-01-16 11:05:19 -0800

    Remove comments

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url='http://github.com/georgestein/cloud-segmentation/',
     author="George Stein",
     author_email='george.f.stein@gmail.com',
-    packages=find_packages(), #['cloud_seg, cloud_seg.pc_apis'], #find_packages(exclude=["tests", "docs", "scripts"]),
+    packages=find_packages(),
     package_data={
         'cloud_seg': [],
         },
