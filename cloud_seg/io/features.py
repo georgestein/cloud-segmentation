ed7784cf08f297ef3962fa3488b3d3233f3b80b0
Dana Simard <dana.simard@astro.caltech.edu>
2022-01-16 11:10:04 -0800

    Bugfixes for colour computations
    Bugfix
    
    Bugfix
    
    Bugfix

--- cloud_seg/io/features.py
+++ cloud_seg/io/features.py
@@ -46,20 +46,9 @@ def add_colour_differences(features, feature_names):
             newfeature = (
                 (features[..., idx1]-features[..., idx2]) /
                 (features[..., idx1]+features[..., idx2]))
-            features = np.concatenate((features, newfeature), axis=-1)
+            features = np.concatenate((features, newfeature[:, np.newaxis]), axis=-1)
             feature_names += [f'{band1}-{band2}']
 
-def add_colour_differences(features, feature_names):
-    """Add colours (normalized differences) to features."""
-    for i, band1 in enumerate(BANDS[:-1]):
-        for band2 in BANDS[i+1:]:
-            idx1 = feature_names.index(band1)
-            idx2 = feature_names.index(band2)
-            newfeature = (
-                (features[..., idx1]-features[..., idx2]) /
-                (features[..., idx1]+features[..., idx2]))
-            features = np.concatenate((features, newfeature), axis=-1)
-            feature_names += [f'{band1}-{band2}']
     return features, feature_names
 
 def add_colour_ratios(features, feature_names):
@@ -69,20 +58,21 @@ def add_colour_ratios(features, feature_names):
             idx1 = feature_names.index(band1)
             idx2 = feature_names.index(band2)
             newfeature = (features[..., idx1]/features[..., idx2])
-            features = np.concatenate((features, newfeature), axis=-1)
+            features = np.concatenate((features, newfeature[:, np.newaxis]), axis=-1)
             feature_names += [f'{band1}/{band2}']
+
     return features, feature_names
 
 def add_intensity(features, feature_names):
     """Add intensity to features."""
-    newfeature = np.zeros((features.shape[0], 1), dtype=features.dtype)
-    for band in enumerate(BANDS):
+    newfeature = np.zeros((features.shape[0]), dtype=features.dtype)
+    for band in BANDS:
         idx = feature_names.index(band)
         newfeature += features[..., idx]
 
     newfeature = newfeature/len(BANDS)
 
-    features = np.concatenate((features, newfeature), axis=-1)
+    features = np.concatenate((features, newfeature[..., np.newaxis]), axis=-1)
     feature_names += ['I']
 
     return features, feature_names
