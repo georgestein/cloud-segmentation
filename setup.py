cf58039ac01cb4da0e1dcbb135a0ad80dde9aef7
Dana Simard <dana.simard@gmail.com>
2022-01-16 10:34:48 -0800

    Fix url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='cloud-segmentation',
     version="0.0",
-    url='http://github.com/gstein/cloud-segmentation/',
+    url='http://github.com/georgestein/cloud-segmentation/',
     author="George Stein",
     author_email='george.f.stein@gmail.com',
     packages=find_packages(), #['cloud_seg, cloud_seg.pc_apis'], #find_packages(exclude=["tests", "docs", "scripts"]),
